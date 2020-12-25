2f59c50eb6224a94bd6157bcffe956b4411d3093
sifatul-teraark <76417921+sifatul-teraark@users.noreply.github.com>
2020-12-25 20:14:42 +0900

    demo page

--- src/App.js
+++ src/App.js
@@ -9,23 +9,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
 
         <SocialShare
             socialTypes={['facebook','twitter','reddit','hatena','instapaper','line','linkedin','livejournal','mailru','ok','pinterest']}
-            url ="aistudios.com"
-            title = "best product ever"
+            url ="http://sifatul.github.io/"
+            title = "Share and get amazing products"
             onSocialButtonClicked={ function (data) {
               console.log('helllooo',data)
             }} 
